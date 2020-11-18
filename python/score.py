854932e2e80b0965d2146ca5694cdb8b5fe7c3ed
vicky-devops <73490219+vicky-devops@users.noreply.github.com>
2020-11-18 13:04:01 -0600

    Update score.py

--- python/score.py
+++ python/score.py
@@ -1,4 +1,3 @@
-
 import json
 import sys
 import joblib
@@ -8,16 +7,15 @@ import numpy as np
 
 def init():
 
-    global path
-    model_path = Model.get_model_path('demodrug')
+    global model
+model_path = Model.get_model_path('demodrug')
     model = joblib.load(model_path)
 
 def run(raw_data):
     try:
-        data = json.loads(raw_data)['data']
-        data = numpy.array(data)
-        result  = model.predict(data)
+        data = np.array(json.loads(raw_data)['data']).astype('float32')
+result  =model.predict(np.array([data]))
         return result.tolist()
     except Exception as e:
         result = str(e)
-        return error
+        return result
